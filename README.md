9c71594ea25565a197920adfbb7c036bd7f87021
Marco Capone <118698117+marcocapone02@users.noreply.github.com>
2023-04-20 02:58:44 +0200

    Milestone 2

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 Boolzapp - a (not very) innovative messaging platform
 
 Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+nome e immagine di ogni contatto
+
+
+Milestone 2
+
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+● Click sul contatto mostra la conversazione del contatto cliccato
---
+++ milestone 2/assets/js/script.js
@@ -0,0 +1,299 @@
+const {createApp} = Vue;
+createApp({
+  
+  data(){
+
+    return{
+
+      contacts:[
+        {
+          name:'Michele',
+          photo:'assets/img/avatar_1.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Ciao',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Ciao che si dice?',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Niente di nuovo',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Bene',
+              status:'received'
+            },
+          ]
+        },
+
+          {
+          name:'Fabio',
+          photo:'assets/img/avatar_2.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Hai avvisato il dottore',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Si',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Menomale',
+              status:'send'
+            },
+          ]
+        },
+
+          {
+          name:'Samuele',
+          photo:'assets/img/avatar_3.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'A che ora vieni?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Non saprei, alle 17?',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Va bene, non fare tardi',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Tranquilla',
+              status:'received'
+            },
+          ]
+        },
+
+          {
+          name:'Alessandro B.',
+          photo:'assets/img/avatar_4.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Ciao, ci sei stasera?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Si, stasera sarà una bella festa',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Anche secondo me',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Non vedo l\'ora',
+              status:'received'
+            },
+
+            
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'A chi lo dici!',
+              status:'send'
+            },
+          ]
+        },
+
+          {
+          name:'Alessandro L.',
+          photo:'assets/img/avatar_5.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Ciao, come stai?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Adesso meglio, grazie',
+              status:'received'
+            },
+          ]
+        },
+
+          {
+          name:'Claudia',
+          photo:'assets/img/avatar_6.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Stasera cinema?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Purtroppo ho una festa',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Sarà per la prossima',
+              status:'send'
+            },
+          ]
+        },
+
+          {
+          name:'Federico',
+          photo:'assets/img/avatar_7.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Perchè non rispondi al telefono?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Al momento sono occupato',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Attendo una tua chiamata',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Va bene',
+              status:'received'
+            },
+          ]
+        },
+
+          {
+          name:'Davide',
+          photo:'assets/img/avatar_8.jpg',
+          visible:true,
+
+          messages:[
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Ciao Davide, ci vediamo stasera?',
+              status:'send'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'Va bene',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'A stasera',
+              status:'received'
+            },
+
+            {
+              date:'20/04/2023',
+              hour: '12:33',
+              message:'A stasera',
+              status:'send'
+            },
+          ]
+        },
+      ],
+
+      counter:0,
+      myMessage:"",
+      
+    }
+  },
+
+methods:{
+  clicked(index){
+    this.counter = index;
+  },
+  newMessage(){
+    const myMessage ={
+      message: this.myMessage,
+      status: 'send'
+    }
+
+    console.log(myMessage)
+    this.contacts.push(myMessage)
+    this.myMessage="";
+  }
+}
+}).mount('#app')
